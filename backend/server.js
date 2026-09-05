e8e32457f772a3ac1822df668eb3bf322508eeeb
yUMIP04 <yumip5719@gmail.com>
2026-09-04 22:39:23 -0600

    feat: estructura inicial de backend y frontend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+/* 🌟 SERVIDOR DE WEBSOCKET*/
+import WebSocket, {WebSocketServer} from "ws";
+
+const ServidorWebsocket = new WebSocketServer( {
+
+    port : 8080,
+    perMessageDeflate: {
+
+        zlibDeflateOptions: {
+            chunkSize:1024,
+            memLevel: 7,
+            level: 3
+        },
+
+        zlibInflateOptions:{
+            chunkSize: 10 * 1024
+        },
+        clientNoContextTakeover:true,
+        serverNoContextTakeover: true,
+        serverMaxWindowBits: 10,
+        concurrencyLimit: 10,
+        threshold: 1024
+    }
+
+
+})
